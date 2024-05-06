7c6515bde247391ed2897734a3422e79374f4962
Aïssa GOSSE <aissagosse@hotmail.com>
2024-05-06 15:23:58 +0200

    resolve conflict2

--- Models/modelCreate.php
+++ Models/modelCreate.php
@@ -1,6 +1,6 @@
 <?php
 function select($mem,$id,$name){
-    $pdo = new PDO('mysql:host=localhost;dbname=donkeyair', 'root', 'manasse22'); 
+    $pdo = new PDO('mysql:host=localhost;dbname=donkeyair', 'root'); 
     $stmt = $pdo->prepare("SELECT * FROM $mem");
     $stmt->execute();
     $countries=$stmt->fetchAll(PDO::FETCH_ASSOC);
