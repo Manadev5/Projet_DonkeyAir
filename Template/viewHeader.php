f601d3f34372d17fb49b4419921b430b0ca395c6
mohamad saidou <mohamad.saidou@outlook.fr>
2024-05-09 10:01:10 +0200

    fixing conflict and css added

--- Template/viewHeader.php
+++ Template/viewHeader.php
@@ -3,57 +3,49 @@
     <head>
         <meta charset="=UTF-8">
         <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/df3c8d82d5.js" crossorigin="anonymous"></script>
     </head>
     <body>
-
-      <h1 class="title">DonkeyAir</h1>
-      <nav>
-            <div class="elements-nav">
-                <ul >
+        <nav>
+            <div class="top">
+                <div class="logo-top-left">
+                    <a class="nav-link" href="index.php"><img src="logo_Donkeyair.png" height="70px" style="margin-top:1%;"></a>
+                </div>
+                <div class="right">                
                     <?php if(isset($_SESSION['admin_log'])){?>
                     
-                    <li class="nav-item">
-                    <a class="nav-link" href="index.php">home</a>
-                    </li>
 
                     <li class="nav-item">
-                    <a class="nav-link" href="pageTickets.php">tickets</a>
+                    <a class="nav-link" href="pageTickets.php"><i class="fa-solid fa-table-list"></i>&#160Tickets</a>
                     </li>
                     
                     <li class="nav-item">
-                    <a class="nav-link" href="pageCreate.php">create</a>
+                    <a class="nav-link" href="pageCreate.php"><i class="fa-solid fa-circle-plus"></i>&#160Create</a>
                     </li>
-                  
+                    
 
                     <li class="nav-item">
-                       <a class="nav-link" href="Controllers/controlLogoutA.php">log-out</a>
+                        <a class="nav-link" href="Controllers/controlLogoutA.php"><i class="fa-solid fa-person-walking-arrow-right"></i>&#160Logout</a>
                     </li>
                     <?php }else if(isset($_SESSION['user_log'])){?>
-                    <li class="nav-item">
-                    <a class="nav-link" href="index.php">home</a>
-                    </li>
                         
                     <li class="nav-item">
-                    <a class="nav-link" href="pageBasket.php">basket</a>     
+                    <a class="nav-link" href="pageBasket.php"><i class="fa-solid fa-basket-shopping"></i>&#160Basket</a>     
                     </li>
 
                     <li class="nav-item">
-                    <a class="nav-link" href="Controllers/controlLogoutU.php">log-out</a>     
+                    <a class="nav-link" href="Controllers/controlLogoutU.php"><i class="fa-solid fa-person-walking-arrow-right"></i>&#160Logout</a>     
                     </li>
                     <?php }else{?>
                     <li class="nav-item">
-                    <a class="nav-link" href="index.php">home</a>
+                    
+                    <a class="nav-link" href="pageAdmin.php"><i class="fa-solid fa-screwdriver-wrench"></i>&#160Admin</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="pageAdmin.php">admin</a>
+                    
+                    <a class="nav-link" href="pageLogin.php"><i class="fa-solid fa-right-to-bracket"></i>&#160Login</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="pageLogin.php">login</a>
-                    </li>
-
-                    <?php } ?>
-                </ul>
-               
+                    <?php } ?></div>
                 </div>
             </div>
        </nav>
