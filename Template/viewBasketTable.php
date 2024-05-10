9bf8960c93e1bb6d118701e952da47fcb9d64779
Manassè MUKENGE <manakabamba@gmail.com>
2024-05-10 11:10:11 +0200

    tickets changes

--- Template/viewBasketTable.php
+++ Template/viewBasketTable.php
@@ -1,21 +1,32 @@
 
 
-            <div>
-                <h3><?=$ticket['country']?></h3>
-                <h3><?=$ticket['des_country']?></h3>
-                <ul>
+<div class="ticket">
+                <section class="ticket-top">
+                    <ul>
+                        <li><?=$ticket['boarding_hour']?></li>
+                        <li><?=$ticket['country']?></li>
+                    </ul>
+                    <ul>
+                        <li><?=$ticket['travel_time']?>h</li>
+
+                    </ul>
+                    
+                    <ul>
+                        <li><?=$ticket['arrival_hour']?></li>
+                        <li><?=$ticket['des_country']?></li>
+                    </ul>
+                    <ul>
+                        <li><?=$ticket['price']?>$</li>
+                    </ul>
+                    
+                </section>
+                
+                <section class="ticket-top">
                     <li><?=$ticket['departure_date']?></li>
                     <li><?=$ticket['arrival_date']?></li>
-                    <li><?=$ticket['boarding_hour']?></li>
-                    <li><?=$ticket['travel_time']?></li>
-                    <li><?=$ticket['arrival_hour']?></li>
-                    <li><?=$ticket['sit_number']?></li>
-                    <li><?=$ticket['travel_number']?></li>
-                    <li><?=$ticket['price']?></li>
-                    
-                </ul>
+                </section>  
                 <a href='Controllers/controlBuy.php?id=<?=$ticket['ticket_id']?>'>buy</a>
-                <a href='Controllers/controlRemoveBasket.php?id=<?=$ticket['ticket_id']?>'>remove</a>
+                <a href='Controllers/controlRemoveBasket.php?id=<?=$ticket['ticket_id']?>'>remove</a>  
             </div>
             <?php
 
--- Template/viewHistory.php
+++ Template/viewHistory.php
@@ -1,18 +1,30 @@
 <?php
 foreach($tickets as $ticket){?>        
-                <div>
-                    <h3><?=$ticket['country']?></h3>
-                    <h3><?=$ticket['des_country']?></h3>
+                <div class="ticket">
+                <section class="ticket-top">
                     <ul>
-                        <li><?=$ticket['departure_date']?></li>
-                        <li><?=$ticket['arrival_date']?></li>
                         <li><?=$ticket['boarding_hour']?></li>
-                        <li><?=$ticket['travel_time']?></li>
-                        <li><?=$ticket['arrival_hour']?></li>
-                        <li><?=$ticket['sit_number']?></li>
-                        <li><?=$ticket['travel_number']?></li>
-                        <li><?=$ticket['price']?></li>
+                        <li><?=$ticket['country']?></li>
                     </ul>
-                </div>
+                    <ul>
+                        <li><?=$ticket['travel_time']?>h</li>
+
+                    </ul>
+                    
+                    <ul>
+                        <li><?=$ticket['arrival_hour']?></li>
+                        <li><?=$ticket['des_country']?></li>
+                    </ul>
+                    <ul>
+                        <li><?=$ticket['price']?>$</li>
+                    </ul>
+                    
+                </section>
+                
+                <section class="ticket-top">
+                    <li><?=$ticket['departure_date']?></li>
+                    <li><?=$ticket['arrival_date']?></li>
+                </section>  
+            </div>
             <?php
                 } 
